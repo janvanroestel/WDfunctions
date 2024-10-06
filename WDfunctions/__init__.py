fd46ec7cc6faf7a4b6b13375e49bdc685cffbd6f
Jan van Roestel <jcjvanroestel@gmail.com>
2024-10-06 22:30:06 +0200

    updated version number

--- WDfunctions/__init__.py
+++ WDfunctions/__init__.py
@@ -1,5 +1,5 @@
 from .WDfunctions import WD_MTR, WD_MR_Eggleton, WD_MR_Nauenberg, WDmagnitudes
 from .WDmagnitudes import WDSED
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __author__ = 'Jan van Roestel'
