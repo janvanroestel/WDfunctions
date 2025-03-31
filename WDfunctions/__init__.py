ff5a73888041c038f912116bea2a57bc3eba71a7
Jan van Roestel <jcjvanroestel@gmail.com>
2025-03-31 15:47:15 +0200

    fixed init file

--- WDfunctions/__init__.py
+++ WDfunctions/__init__.py
@@ -1,4 +1,4 @@
-from .WDfunctions import WD_MTR, WD_MR_Eggleton, WD_MR_Nauenberg
+from .WDfunctions import WD_MTR, WD_MTage, WD_MR_Eggleton, WD_MR_Nauenberg
 from .WDmagnitudes import WDSED
 DAmags = WDSED()
 
